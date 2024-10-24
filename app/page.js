62df9fe432847dd79661d94c593f76aa6ebbda8e
FurkannOzbek <furkannozbek@yandex.com>
2024-10-24 15:05:59 +0200

    skeleton strcuture has been created and icon has been added

--- app/page.js
+++ app/page.js
@@ -1,6 +1,11 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import RateCard from "./Components/RateCard";
 
 export default function Home() {
-  return <p>asd</p>;
+  return (
+    <>
+      <RateCard />
+    </>
+  );
 }
